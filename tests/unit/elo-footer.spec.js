0f6dc2fc661c81381f35f2882559446545801130
Douglas Wade <douglas.b.wade@gmail.com>
2022-03-22 00:42:27 -0700

    Is this a bug?

--- tests/unit/elo-footer.spec.js
+++ tests/unit/elo-footer.spec.js
@@ -1,8 +1,9 @@
 import { shallowMount } from '@vue/test-utils'
+import { createVuetify } from 'vuetify'
 import EloFooter from '@/components/EloFooter.vue'
 
 describe('EloFooter.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('renders authorship', () => {
     const wrapper = shallowMount(EloFooter)
 
     expect(wrapper.text()).toMatch('Doug Wade 2022')
--- tests/unit/elo-header.spec.js
+++ tests/unit/elo-header.spec.js
@@ -1,15 +1,18 @@
 import { shallowMount, mount } from '@vue/test-utils'
+import { createVuetify } from 'vuetify'
 import EloHeader from '@/components/EloHeader.vue'
+
+const plugins = [Vuetify]
 
 describe('EloHeader.vue', () => {
   it('renders', () => {
-    const wrapper = shallowMount(EloHeader)
+    const wrapper = shallowMount(EloHeader, { plugins })
 
     expect(wrapper.text()).toMatch('Menu')
-  })``
+  })
 
   it('emits when the button is clicked', async () => {
-    const wrapper = mount(EloHeader)
+    const wrapper = mount(EloHeader, { plugins })
 
     const button = wrapper.find('button')
     await button.trigger('click')
